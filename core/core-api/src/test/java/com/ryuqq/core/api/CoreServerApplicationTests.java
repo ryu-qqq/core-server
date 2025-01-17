cf6e0a66e39ee842e77ac980749d540e15096abc
RyuSangwon <rsw2@connectly.co.kr>
2025-01-17 17:40:17 +0900

    운영 설정 및 도커파일 추가

--- core/core-api/src/test/java/com/ryuqq/core/api/CoreServerApplicationTests.java
+++ core/core-api/src/test/java/com/ryuqq/core/api/CoreServerApplicationTests.java
@@ -3,9 +3,11 @@ package com.ryuqq.core.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @ConfigurationPropertiesScan
 @SpringBootTest
+@ActiveProfiles("test")
 class CoreServerApplicationTests {
 
     @Test
