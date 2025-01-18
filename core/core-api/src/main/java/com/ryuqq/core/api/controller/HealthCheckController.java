0f522b0017983dfa5bf0ad99089d0d034c0a8355
RyuSangwon <rsw2@connectly.co.kr>
2025-01-18 14:16:17 +0900

    헬스체크 추가

--- core/core-api/src/main/java/com/ryuqq/core/api/controller/HealthCheckController.java
+++ core/core-api/src/main/java/com/ryuqq/core/api/controller/HealthCheckController.java
@@ -1,11 +1,11 @@
 package com.ryuqq.core.api.controller;
 
-import com.ryuqq.core.api.payload.ApiResponse;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ryuqq.core.api.payload.ApiResponse;
 
 
 @RestController
