40c8531faf5a16e69ead9b2174f5fb657a3904f3
RyuSangwon <rsw2@connectly.co.kr>
2025-01-30 16:43:38 +0900

    product module init

--- external-client/buyma/src/main/java/com/ryuqq/core/external/buyma/mapper/BuyMaProductMapper.java
+++ external-client/buyma/src/main/java/com/ryuqq/core/external/buyma/mapper/BuyMaProductMapper.java
@@ -18,27 +18,16 @@ public class BuyMaProductMapper {
 
 	public BuyMaProductInsertRequestWrapperDto toInsetRequestDto(ExternalProductGroup externalProductGroup){
 
+
+		// return BuyMaProductInsertFactory.createInsertRequestDto(
+		// 	externalProductGroup,
+		//
+		//
+		//
+		// );
+
 		return null;
 	}
-
-
-	// private CommonContext prepareCommonContext(ExternalProductGroup externalProductGroup) {
-	// 	ItemContext itemContext = productGroupContextQueryInterface.fetchByProductGroupId(externalProductGroup.getProductGroupId());
-	// 	Item item = itemContext.getItem();
-	// 	ItemNoticeInfo noticeInfo = itemContext.getNoticeInfo();
-	// 	ItemDeliveryInfo deliveryInfo = itemContext.getDeliveryInfo();
-	// 	BigDecimal buyMaPrice = BuyMaPriceHelper.calculateFinalPrice(item.getPrice().getRegularPrice(),
-	// 		BigDecimal.valueOf(934.56));
-	// 	List<BuyMaImageInsertRequestDto> buyMaImages = BuyMaImageInsertFactory.toBuyMaImages(
-	// 		itemContext.getItemImages());
-	// 	List<OcoOptionContext> optionContexts = ocoOptionConverter.generateOptionContext(
-	// 		externalProductGroup.getProductGroupId(), externalProductGroup.getExternalProductGroupId());
-	// 	String externalCategoryId = resolveCategoryId(externalProductGroup);
-	//
-	// 	return new CommonContext(item, noticeInfo, deliveryInfo, price, images,
-	// 		itemContext.getItemDescription(), optionContexts, externalCategoryId);
-	// }
-
 
 
 }
